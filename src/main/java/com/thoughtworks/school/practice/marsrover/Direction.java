8c97e1840da434bfafaf0659e47fd6ee9397dc27
kbyyd24 <melo@gaoyuexiang.cn>
2020-01-12 17:35:04 +0800

    refactor, implement turn in each Direction

--- src/main/java/com/thoughtworks/school/practice/marsrover/Direction.java
+++ src/main/java/com/thoughtworks/school/practice/marsrover/Direction.java
@@ -1,46 +1,66 @@
 package com.thoughtworks.school.practice.marsrover;
 
+import static com.thoughtworks.school.practice.marsrover.TurnDirection.L;
+
 public enum Direction {
   N {
     @Override
     public Position move(Position previousPosition) {
       return previousPosition.moveY(1);
     }
-  }, E {
+
+    @Override
+    public Direction turn(TurnDirection turnDirection) {
+      if (turnDirection == L) {
+        return W;
+      }
+      return null;
+    }
+  },
+  E {
     @Override
     public Position move(Position previousPosition) {
       return previousPosition.moveX(1);
     }
-  }, S {
+
+    @Override
+    public Direction turn(TurnDirection turnDirection) {
+      if (turnDirection == L) {
+        return N;
+      }
+      return null;
+    }
+  },
+  S {
     @Override
     public Position move(Position previousPosition) {
       return previousPosition.moveY(-1);
     }
-  }, W {
+
+    @Override
+    public Direction turn(TurnDirection turnDirection) {
+      if (turnDirection == L) {
+        return E;
+      }
+      return null;
+    }
+  },
+  W {
     @Override
     public Position move(Position previousPosition) {
       return previousPosition.moveX(-1);
+    }
+
+    @Override
+    public Direction turn(TurnDirection turnDirection) {
+      if (turnDirection == L) {
+        return S;
+      }
+      return null;
     }
   };
 
   abstract public Position move(Position previousPosition);
 
-  public Direction turn(TurnDirection turnDirection) {
-    if (turnDirection == TurnDirection.L) {
-      if (this == N) {
-        return W;
-      }
-      if (this == W) {
-        return S;
-      }
-      if (this == S) {
-        return E;
-      }
-      if (this == E) {
-        return N;
-      }
-    }
-    return null;
-  }
-
+  abstract public Direction turn(TurnDirection turnDirection);
 }
